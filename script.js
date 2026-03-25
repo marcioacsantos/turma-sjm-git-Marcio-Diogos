4b43571db4c88737846fe1b5143e15644393e70f
DiogoOliveira37 <diogooliveira.escola@hotmail.com>
2026-03-25 11:56:54 +0000

    Validar campo nome

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const nome = document.getElementById("nome");
 console.log("JS ligado");
 const form = document.querySelector("form");
 form.addEventListener("submit", function(e) {
 e.preventDefault();
+if (nome.value === "") {
+    alert("Preencha o nome");
+}
 });
